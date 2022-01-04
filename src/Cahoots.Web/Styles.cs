d1ce238510a16c2ce781d4a8ff6e8ae7ca54d558
Jorge Cano <jorgecano86@gmail.com>
2022-01-04 21:07:14 +0100

    Working web

--- src/Cahoots.Web/Styles.cs
+++ src/Cahoots.Web/Styles.cs
@@ -1,7 +1,11 @@
 ﻿public class Styles
 {
     public const string Link = "text-xs font-bold text-indigo-600 hover:text-indigo-800 lg:text-base";
-    public static string Title = $"text-slate-700 font-semibold uppercase text-xl xl:text-4xl";
-    public static string SubTitle = $"text-sm text-slate-600 tracking-widest lg:text-lg";
-    public static string Container = $"h-full flex justify-center p-8";
+    public const string Title = $"text-slate-700 font-semibold uppercase text-xl xl:text-4xl";
+    public const string SubTitle = $"text-sm text-slate-600 tracking-widest lg:text-lg";
+    public const string Container = $"h-full flex justify-center p-8";
+    public const string PrimaryButton = "bg-slate-700  hover:bg-slate-500 text-white " + Button;
+    public const string SecondaryButton = "bg-slate-200 hover:bg-slate-300 text-slate-700 " + Button;
+    private const string Button = $"font-semibold outline-none focus:outline-none rounded-xl" +
+        $" w-full p-2 md:p-4 md:text-xl md:text-2xl uppercase outline-none focus:outline-none flex items-center justify-center";
 }
--- src/Core/Game/GameOptions.cs
+++ src/Core/Game/GameOptions.cs
@@ -3,14 +3,12 @@ namespace Cahoots.Core
     public class GameOptions
     {
         public int HandSize { get; set; }
-        public int MissionCount { get; set; }
-        public int PileCount { get; set; }
+        public int MissionCount = 4;
+        public int PileCount = 4;
 
         public GameOptions()
         {
             HandSize = 4;
-            MissionCount = 4;
-            PileCount = 4;
         }
     }
 }
